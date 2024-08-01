9bb9d4758c97c8187e7b3db09e8367dc87c224ec
shraddha1253 <shraddhamore5587@gmail.com>
2024-08-01 09:35:53 +0530

    02-typeof.js
    02-typeof.js

---
+++ 02-typeOf.js
@@ -0,0 +1,43 @@
+
+// Primitive Datatype: Number, String and Boolean
+console.log("============= 1 ===============");
+console.log("bank value is: ", bankTypeOf);
+var bank= 'sbi bank';
+console.log("bank is:", bank);
+var bankTypeOf = typeof bank;
+console.log("Type of bank is:", bankTypeOf);
+
+console.log( "============2=============")
+
+var marks = "95";
+console.log("Mark is: ", marks);
+var marksTypeOf = typeof marks;
+var marksTypeOf = typeof marks;
+console.log("Type of mark is:", marksTypeOf);
+
+console.log("============3===========")
+var isWorkingstatus= true;
+console.log("my Working status : ", isWorkingstatus);
+var isWorkingstatusTypeOf = typeof isWorkingstatus;
+console.log("my Working status type of is: ", isWorkingstatusTypeOf);
+
+
+console.log("==================4===========")
+
+var totalcountishundredandseven = "";
+console.log("total count is hundred and seven is: ", totalcountishundredandseven);
+var TypeOf = typeof marks;
+var totalcountishundredandsevenTypeOf = typeof totalcountishundredandseven;
+console.log("Type of total count data type is :", totalcountishundredandsevenTypeOf);
+var updatednumber = 107;
+console.log("updated number is:",updatednumber );
+var updatednumberTypeOf = typeof age;
+console.log("Datatype of updated number is: ", updatednumberTypeOf);
+ 
+
+
+
+
+
+
+

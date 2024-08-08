5dbd3bf8a0f03e63c983754cf377e26af36d4cea
shraddha1253 <shraddhamore5587@gmail.com>
2024-08-08 09:59:19 +0530

    test.js
    test.js

---
+++ javascript-programs/test.js
@@ -0,0 +1,5 @@
+console.log("Hello,i am shraddha");
+console.log("my city name is Akluj");
+console.log("My Best Friend Name is Pravin");
+console.log("My school name is smv akluj");
+console.log("my college name is D.Y.patil IPSR pune")

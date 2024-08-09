7a6ad93bc580c02a4e56fe578c60bca1733b52d4
shraddha1253 <shraddhamore5587@gmail.com>
2024-08-09 10:04:00 +0530

    emplictAssign.js
    emplictAssign.js

---
+++ javascript02-program/emplictAssign.js
@@ -0,0 +1,31 @@
+console.log(`========= Implicit Conversion ==============`);
+
+console.log('3' + 2);
+console.log(`
+    the type of "3" is string type 
+     the type of 2 is  number 
+    so the output operand string and number converted to string got concatenated`);
+
+
+
+
+
+console.log('3' + true);
+console.log(`
+    the type of "3" is string type 
+     the type of true is  number 
+    so the output operand string and boolean converted to string got concatenated`);
+
+console.log('3'+undefined);
+
+console.log(`
+    the type of "3" is string type 
+     the type of  undefined is  number 
+    so the output operand string and undefined converted to string got concatenated`);
+
+console.log('3' + null);
+
+console.log(`
+    the type of "3" is string type 
+     the type of null is  number 
+    so the output operand string and null converted to string got concatenated`);

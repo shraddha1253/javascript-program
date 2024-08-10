77b1096366f326bd9c7b92f9318f54dae5a427ff
shraddha1253 <shraddhamore5587@gmail.com>
2024-08-10 18:23:17 +0530

    06-for-loopAssignment
    06-for-loopAssignment

---
+++ javascript02-program/06-for-loop assig.js
@@ -0,0 +1,45 @@
+
+
+console.log(`======= Print even numbers from 5 to 15 ==========`);
+for (let index = 5; index <= 15; index=index+1) {
+    console.log(index);
+}
+
+console.log(`======= find  the 15 odd number ==========`);
+for (let index = 1; index <= 29; index=index+2) {
+    console.log(index);
+}
+
+console.log(`======= the numbers from 50 to 40 ==========`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+
+
+
+
+console.log(`======= Print even numbers from 2 to 20 ==========`);
+for (let index = 2; index <= 20; index=index+2) {
+    console.log(index);
+}
+
+
+console.log(`======= Print even numbers from 5 to 50==========`);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+
+console.log(`======= Print even numbers from 10 to 100==========`);
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);
+}
+
+
+
+
+console.log(`=======reverse 10 ==========`);
+for (let index = 10; index >= 100; index=index-5) {
+    console.log(index);  
+}
